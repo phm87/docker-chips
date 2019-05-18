fa510677520a6af37552ae23eebbc0a98ea31759
phm87 <31578435+phm87@users.noreply.github.com>
2019-05-18 18:12:46 +0200

    add Bootstrap

--- entry.sh
+++ entry.sh
@@ -17,6 +17,16 @@ txindex=1
 rpcallowip=127.0.0.1
 rpcallowip=${listenip:-127.0.0.1}
 addnode=5.9.253.195
+rpcport=12454
+# port=57777
+addnode=54.36.126.42
+addnode=145.239.149.173
+addnode=54.39.53.170
+addnode=159.69.23.30
+addnode=95.213.238.98
+addnode=144.217.10.241
+addnode=5.189.232.34
+addnode=139.99.125.27
 
 EOF
 
@@ -38,9 +48,18 @@ else
 
 fi
 
-echo
 echo "****************************************************"
-echo "Running: chipsd ${args[@]}"
+echo "Download bootstrap"
+echo "****************************************************"
+mkdir -p ~/.chips
+cd ~/.chips
+wget http://bootstrap3rd.dexstats.info/CHIPS-bootstrap.tar.gz
+tar xvzf CHIPS-bootstrap.tar.gz
+rm CHIPS-bootstrap.tar.gz
+echo "****************************************************"
+echo "Bootstrap downloaded"
+echo "****************************************************"
+echo "Running: chipsd -reindex"
 echo "****************************************************"
 
-exec ~/chips3/src/chipsd ${args[@]}
+exec ~/chips3/src/chipsd -reindex
